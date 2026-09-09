8d17e968cb87217062d739af21c162c703985ca8
TitaniusG <292889056+TitaniusG@users.noreply.github.com>
2026-09-09 19:01:06 +0200

    Added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 5173,
+  host: true,
+  open: false,
+  allowedHosts: true,
+  cors: true,
+  headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+});
